96015c496e0c21fe2a17b73a6dc518e6ab2f5cca
jakshybala <maximuspro.kg@gmail.com>
2022-05-24 22:45:13 +0600

    commit first

---
+++ src/grey/company/Main.java
@@ -0,0 +1,10 @@
+package grey.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("My first commit in github");
+    }
+}
